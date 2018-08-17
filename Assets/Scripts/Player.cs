c10cb67c01ea14f1f4d20272894cab4024488d9c
ZeroTheDeathless <42423959+ZeroTheDeathless@users.noreply.github.com>
2018-08-17 10:15:52 +1000

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CTRL + K + D (Cleans code)
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpHeight = 10f;
+    public Rigidbody rigid;
+    public float rayDistance = 1f;
+    private void OnDrawGizmos()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundRay.origin, groundRay.origin + groundRay.direction * rayDistance);
+    }
+
+    bool IsGrounded()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(groundRay, out hit, rayDistance))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal") * moveSpeed;
+        float inputV = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 moveDir = new Vector3(inputH, 0f, inputV);
+        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            force.y = jumpHeight;
+        }
+        
+        rigid.velocity = force;
+
+        if (moveDir.magnitude > 0)
+
+        {
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+    }
+}
